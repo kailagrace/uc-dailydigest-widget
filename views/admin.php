b7a45d384538130b2f2c4e3f78a957db5d3ad24c
Joseph Thibeault <joseph.thibeault@gmail.com>
2014-05-13 15:52:28 -0400

    Modified boilerplate, started adding form elements

--- views/admin.php
+++ views/admin.php
@@ -1,1 +1,4 @@
-<!-- This file is used to markup the administration form of the widget. -->
+<p>
+    <label for="<?php echo $this->get_field_id( 'feed_url' ); ?>">Select Feed:</label><br/>
+    <input class="widefat" type="text" value="<?php echo $instance['feed_url']; ?>" id="<?php echo $this->get_field_id( 'feed_url' ); ?>" name="<?php echo $this->get_field_name( 'feed_url' ); ?>"/>
+</p>
