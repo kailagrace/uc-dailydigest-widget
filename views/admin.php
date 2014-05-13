e01a170dcfbd1e9ffe54b5656e19dd8ca2cacc70
Joseph Thibeault <joseph.thibeault@gmail.com>
2014-05-13 18:12:22 -0400

    Added daily digest feed selector, and number of posts field

--- views/admin.php
+++ views/admin.php
@@ -1,4 +1,20 @@
 <p>
     <label for="<?php echo $this->get_field_id( 'feed_url' ); ?>">Select Feed:</label><br/>
-    <input class="widefat" type="text" value="<?php echo $instance['feed_url']; ?>" id="<?php echo $this->get_field_id( 'feed_url' ); ?>" name="<?php echo $this->get_field_name( 'feed_url' ); ?>"/>
+    <select class="widefat" id="<?php echo $this->get_field_id( 'feed_url' ); ?>" name="<?php echo $this->get_field_name( 'feed_url' ); ?>">
+
+        <?php
+        foreach($feed_urls as $name => $url):
+        ?>
+        <option value="<?php echo $url; ?>" <?php if($feed_url == $url) echo 'selected="selected"'; ?>>
+            <?php echo $name; ?>
+        </option>
+        <?php
+        endforeach;
+        ?>
+
+    </select>
+</p>
+<p>
+    <label for="<?php echo $this->get_field_id( 'num_posts' ); ?>">Number of posts to show:</label> 
+    <input type="text" size="3" name="<?php echo $this->get_field_name( 'num_posts' ); ?>" id="<?php echo $this->get_field_id( 'num_posts' ); ?>" value="<?php echo $num_posts; ?>"/>
 </p>
