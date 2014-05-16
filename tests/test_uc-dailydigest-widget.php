4c7ea5a70659a35acc2718a29085e631d7054c34
Joseph Thibeault <joseph.thibeault@gmail.com>
2014-05-16 13:22:46 -0400

    Syntax errors in test code

--- tests/test_uc-dailydigest-widget.php
+++ tests/test_uc-dailydigest-widget.php
@@ -10,23 +10,23 @@ class WP_Test_UC_DailyDigest_Widget extends WP_UnitTestCase {
     }
 
     function test_init_hook_was_added() {
-        $this->assertGreaterThan( 0, has_filter( 'init', 'widget_textdomain');
+        $this->assertGreaterThan( 0, has_filter( 'init', 'widget_textdomain') );
     }
 
     function test_admin_styles_registered() {
-        $this->assertTrue( wp_style_is( 'register_admin_styles', 'registered'));
+        $this->assertTrue( wp_style_is( 'register_admin_styles', 'registered' ) );
     }
 
     function test_admin_styles_enqueued() {
-        $this->assertTrue( wp_style_is( 'register_admin_styles', 'enqueued'));
+        $this->assertTrue( wp_style_is( 'register_admin_styles', 'enqueued') );
     }
 
     function test_widget_styles_registered() {
-        $this->assertTrue( wp_style_is( 'register_widget_styles', 'registered'));
+        $this->assertTrue( wp_style_is( 'register_widget_styles', 'registered') );
     }
 
     function test_widget_styles_enqueued() {
-        $this->assertTrue( wp_style_is( 'register_widget_styles', 'enqueued'));
+        $this->assertTrue( wp_style_is( 'register_widget_styles', 'enqueued') );
     }
 
     function test_get_widget_slug() {
