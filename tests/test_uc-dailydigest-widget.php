6a60f69a03b842ee44a9b76c326db86909f2fae7
Joseph Thibeault <joseph.thibeault@gmail.com>
2014-05-16 14:23:23 -0400

    Updating tests

--- tests/test_uc-dailydigest-widget.php
+++ tests/test_uc-dailydigest-widget.php
@@ -1,6 +1,18 @@
 <?php
 class WP_Test_UC_DailyDigest_Widget extends WP_UnitTestCase {
 
+    function setUp() {
+        global $uc_dailydigest_widget;
+
+        parent::setUp();
+
+        include_once( plugin_dir_path( dirname( __FILE__ ) ) . '/uconn-daily-digest-widget.php' );
+
+        // Allow for a plugin to insert a different class to handle requests.
+        $uc_dailydigest_widget_class = apply_filters('uc_dailydigest_widget_class', 'UConn_Daily_Digest_Widget');
+        $uc_dailydigest_widget = new $uc_dailydigest_widget_class;
+        }
+
     function test_tests() {
         $this->assertTrue( true );
     }
@@ -10,7 +22,9 @@ class WP_Test_UC_DailyDigest_Widget extends WP_UnitTestCase {
     }
 
     function test_init_hook_was_added() {
-        $this->assertGreaterThan( 0, has_filter( 'init', 'widget_textdomain') );
+        global $uc_dailydigest_widget;
+        
+        $this->assertGreaterThan( 0, has_filter( 'init', array( $uc_dailydigest_widget, 'widget_textdomain') ) );
     }
 
     function test_admin_styles_registered() {
