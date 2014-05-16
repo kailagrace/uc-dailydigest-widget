78642103611dcb476c7465a2e486765327cb8459
Joseph Thibeault <joseph.thibeault@gmail.com>
2014-05-16 11:16:45 -0400

    More test matrix mods

--- tests/test_uc-dailydigest-widget.php
+++ tests/test_uc-dailydigest-widget.php
@@ -9,8 +9,20 @@ class WP_Test_UC_DailyDigest_Widget extends WP_UnitTestCase {
         $this->assertGreaterThan( 0, has_filter( 'init', array($this, 'widget_textdomain'));
     }
 
+    function test_admin_styles_registered() {
+        $this->assertTrue( wp_style_is( 'register_admin_styles', 'registered'));
+    }
+
     function test_admin_styles_enqueued() {
         $this->assertTrue( wp_style_is( 'register_admin_styles', 'enqueued'));
+    }
+
+    function test_widget_styles_registered() {
+        $this->assertTrue( wp_style_is( 'register_widget_styles', 'registered'));
+    }
+
+    function test_widget_styles_enqueued() {
+        $this->assertTrue( wp_style_is( 'register_widget_styles', 'enqueued'));
     }
 }
 ?>
