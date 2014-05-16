70315ebe180feeb9d622a96c1a3bbb7045f7eb5c
Joseph Thibeault <joseph.thibeault@gmail.com>
2014-05-16 11:37:29 -0400

    testing build

--- tests/test_uc-dailydigest-widget.php
+++ tests/test_uc-dailydigest-widget.php
@@ -6,7 +6,7 @@ class WP_Test_UC_DailyDigest_Widget extends WP_UnitTestCase {
     }
 
     function test_init_hook_was_added() {
-        $this->assertGreaterThan( 0, has_filter( 'init', array($this, 'widget_textdomain'));
+        $this->assertGreaterThan( 0, has_filter( 'init', 'widget_textdomain');
     }
 
     function test_admin_styles_registered() {
