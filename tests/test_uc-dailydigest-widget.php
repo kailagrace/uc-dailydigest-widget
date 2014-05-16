90b66a4e9da5a26463b418a0cf28d2c1b74ac4e4
Joseph Thibeault <joseph.thibeault@gmail.com>
2014-05-16 16:19:37 -0400

    Fixed tests

--- tests/test_uc-dailydigest-widget.php
+++ tests/test_uc-dailydigest-widget.php
@@ -28,6 +28,9 @@ class WP_Test_UC_DailyDigest_Widget extends WP_UnitTestCase {
 
     function test_admin_styles_enqueued() {
         global $uc_dailydigest_widget;
+        var_dump(wp_style_is(
+            'register_admin_styles'
+        , 'enqueued' ));
         $this->assertTrue( wp_style_is(
             'register_admin_styles'
         , 'enqueued' ) );
