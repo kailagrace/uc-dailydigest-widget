63ea6556d97ca0a4f4b2bfe2136c5f6af6775473
Joseph Thibeault <joseph.thibeault@gmail.com>
2014-05-16 14:28:54 -0400

    Updating tests

--- tests/test_uc-dailydigest-widget.php
+++ tests/test_uc-dailydigest-widget.php
@@ -23,29 +23,44 @@ class WP_Test_UC_DailyDigest_Widget extends WP_UnitTestCase {
 
     function test_init_hook_was_added() {
         global $uc_dailydigest_widget;
-        
         $this->assertGreaterThan( 0, has_filter( 'init', array( $uc_dailydigest_widget, 'widget_textdomain') ) );
     }
 
     function test_admin_styles_registered() {
-        $this->assertTrue( wp_style_is( 'register_admin_styles', 'registered' ) );
+        global $uc_dailydigest_widget;
+        $this->assertTrue( wp_style_is( array(
+            $uc_dailydigest_widget,
+            'register_admin_styles'
+        ), 'registered' ) );
     }
 
     function test_admin_styles_enqueued() {
-        $this->assertTrue( wp_style_is( 'register_admin_styles', 'enqueued') );
+        global $uc_dailydigest_widget;
+        $this->assertTrue( wp_style_is( array(
+            $uc_dailydigest_widget,
+            'register_admin_styles'
+        ), 'enqueued' ) );
     }
 
     function test_widget_styles_registered() {
-        $this->assertTrue( wp_style_is( 'register_widget_styles', 'registered') );
+        global $uc_dailydigest_widget;
+        $this->assertTrue( wp_style_is( array(
+            $uc_dailydigest_widget,
+            'register_widget_styles'
+        ), 'registered' ) );
     }
 
     function test_widget_styles_enqueued() {
-        $this->assertTrue( wp_style_is( 'register_widget_styles', 'enqueued') );
+        global $uc_dailydigest_widget;
+        $this->assertTrue( wp_style_is( array(
+            $uc_dailydigest_widget,
+            'register_widget_styles'
+        ), 'enqueued' ) );
     }
 
     function test_get_widget_slug() {
-        global $wp_widget_factory;
-        $this->assertEquals( 'uconn-daily-digest-widget', $wp_widget_factory->widgets['UConn_Daily_Digest_Widget'] );
+        global $uc_dailydigest_widget;
+        $this->assertEquals( 'uconn-daily-digest-widget', $uc_dailydigest_widget->get_widget_slug() );
     }
 }
 ?>
