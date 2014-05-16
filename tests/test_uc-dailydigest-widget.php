93adf6d94824bc4b00edd4a5b42db3c4682ba8a3
Joseph Thibeault <joseph.thibeault@gmail.com>
2014-05-16 11:12:45 -0400

    Unit tests

--- tests/test_uc-dailydigest-widget.php
+++ tests/test_uc-dailydigest-widget.php
@@ -1,5 +1,16 @@
 <?php
-// class WP_Test_WordPress_Plugin_Tests extends WP_UnitTestCase {
-//
-// }
+class WP_Test_UC_DailyDigest_Widget extends WP_UnitTestCase {
+
+    function test_tests() {
+        $this->assertTrue( true );
+    }
+
+    function test_init_hook_was_added() {
+        $this->assertGreaterThan( 0, has_filter( 'init', array($this, 'widget_textdomain'));
+    }
+
+    function test_admin_styles_enqueued() {
+        $this->assertTrue( wp_style_is( 'register_admin_styles', 'enqueued'));
+    }
+}
 ?>
